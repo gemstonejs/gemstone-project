62460e1bb6dd07110d699c94d5349bcc28bbb763
Ralf S. Engelschall <rse@engelschall.com>
2017-04-05 16:42:18 +0200

    safe some useful scripts for developers

---
+++ etc/1-clone-repositories.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+##
+##  GemstoneJS -- Gemstone JavaScript Technology Stack
+##  Copyright (c) 2016-2017 Gemstone Project <http://gemstonejs.com>
+##  Licensed under Apache License 2.0 <https://spdx.org/licenses/Apache-2.0>
+##
+
+basedir=`echo "$0" | sed -e 's;/[^/]*$;;'`
+username=`whoami`
+scheme="https"
+if [ ".`egrep \"^$username .*DEVELOPER\" $basedir/0-manifest.txt`" != . ]; then
+    scheme="ssh"
+fi
+for repo in `grep REPO $basedir/0-manifest.txt | sed -e 's; .*$;;'`; do
+    url="$scheme://github.com/gemstonejs/$repo"
+    if [ -d $repo ]; then
+        echo "++ cloning: $repo <- $url [ALREADY EXISTS - SKIPPING]"
+    else
+        echo "++ cloning: $repo <- $url"
+        git clone $url
+    fi
+done
+
---
+++ etc/2-symlink-packages.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+##
+##  GemstoneJS -- Gemstone JavaScript Technology Stack
+##  Copyright (c) 2016-2017 Gemstone Project <http://gemstonejs.com>
+##  Licensed under Apache License 2.0 <https://spdx.org/licenses/Apache-2.0>
+##
+
+basedir=`echo "$0" | sed -e 's;/[^/]*$;;'`
+for pkg in `grep PKG $basedir/0-manifest.txt | sed -e 's; .*$;;'`; do
+    echo "++ processing $pkg"
+    (   cd $pkg
+        if [ ! -f package.json ]; then
+            echo "** ERROR: no package.json found"
+            exit 1
+        fi
+        if [ -d node_modules ]; then
+            rm -rf node_modules
+        fi
+        upd
+        npm install
+        (   cd node_modules || exit $?
+            for mod in gemstone-*; do
+                if [ ".$mod" = ".gemstone-*" ]; then
+                    continue
+                fi
+                echo "-- $pkg: symlinking $mod"
+                rm -rf ./$mod
+                ln -s ../../$mod $mod
+            done
+        )
+    )
+done
+
