d15e8c5362418826a788ff061e83dc1b7025f8c5
Ralf S. Engelschall <rse@engelschall.com>
2017-06-23 23:08:09 +0200

    ignore webpack update for now

--- etc/2-symlink-packages.sh
+++ etc/2-symlink-packages.sh
@@ -16,7 +16,7 @@ for pkg in `grep PKG $basedir/0-manifest.txt | sed -e 's; .*$;;'`; do
         if [ -d node_modules ]; then
             rm -rf node_modules
         fi
-        upd '!postcss*' '!precss' '!autoprefixer'
+        upd '!postcss*' '!precss' '!autoprefixer' '!webpack'
         npm install
 		if [ -d node_modules ]; then
 			(   cd node_modules || exit $?
